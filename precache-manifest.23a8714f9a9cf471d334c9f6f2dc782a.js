ff93a5f047b45f3875be3d85171098a56562be74
Vikas <44545660+vikasdo@users.noreply.github.com>
2020-04-25 13:10:46 +0530

    Updates

---
+++ precache-manifest.23a8714f9a9cf471d334c9f6f2dc782a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de254bb66d1beb4c837494723a27a8e2",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "b2a5df50023da5ffa371",
+    "url": "/portfolio/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "ca1dbd93eb93ca6262bf",
+    "url": "/portfolio/static/js/2.3367e9ca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.3367e9ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2a5df50023da5ffa371",
+    "url": "/portfolio/static/js/main.67670b0a.chunk.js"
+  },
+  {
+    "revision": "f619aeccc94a0d089131",
+    "url": "/portfolio/static/js/runtime-main.075c0788.js"
+  },
+  {
+    "revision": "c2cb4bf14cfd9bbe381438cf36b7a22d",
+    "url": "/portfolio/static/media/profile.c2cb4bf1.jpg"
+  }
+]);
